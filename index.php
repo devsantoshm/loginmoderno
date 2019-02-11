609bc5e1110bb0cd6db4d1e3c739b3e26daff74d
SantosHM <dosantoshm@gmail.com>
2019-02-11 18:35:05 -0500

    inicio

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1 shrink-to-fit=no">
+	<title>Signup & Login</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+	
+	<nav class="navbar navbar-expand-sm navbar-light bg-light">
+		<div class="container">
+			<a href="#" class="navbar-brand">Signup & Login</a>
+			<button type="button" class="navbar-toggler" data-target="#mynav" data-toggle="collapse">
+				<span class="navbar-toggler-icon"></span>
+				
+			</button><!-- button -->
+			<div class="collapse navbar-collapse" id="mynav">
+
+				 <ul class="navbar-nav ml-auto">
+				 	<li class="nav-item">
+				 		<a href="#" class="nav-link">Home</a>
+				 	</li>
+				 </ul>
+			</div><!-- collapse -->
+		</div><!-- container -->
+	</nav><!-- nav close -->
+
+<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
