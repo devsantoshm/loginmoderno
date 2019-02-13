263232c140af59ba7952c00d9e7ab980831e91a4
SantosHM <dosantoshm@gmail.com>
2019-02-12 19:32:29 -0500

    creando el form

--- index.php
+++ index.php
@@ -27,6 +27,48 @@
 			</div><!-- collapse -->
 		</div><!-- container -->
 	</nav><!-- nav close -->
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 content">
+				<h1>Its always free</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga modi neque, harum, autem illum, commodi voluptatibus sequi debitis enim obcaecati animi amet libero ipsa tempora atque rem qui iste aliquam?</p>
+			</div>
+			<div class="col-md-4 content">
+				<div class="card">
+					<div class="card-header">
+						<div class="row">
+							<div class="col-md-9">
+								<h3 class="form-heading">Signup</h3>
+								<p>Account creating is 100% free so please create the account</p>
+							</div>
+							<div class="col-md-3 text-right">
+								<i class="fa fa-pencil-square-o fa-3x"></i>
+							</div>
+						</div>
+					</div>
+					<div class="card-body">
+						<form>
+							<div class="form-group">
+								<input type="text" id="name" class="form-control" placeholder="name">
+							</div>
+							<div class="form-group">
+								<input type="email" id="email" class="form-control" placeholder="email">
+							</div>
+							<div class="form-group">
+								<input type="password" id="password" class="form-control" placeholder="password">
+							</div>
+							<div class="form-group">
+								<input type="password" id="confirm" class="form-control" placeholder="Confirm Password">
+							</div>
+							<div class="form-group">
+								<button type="button" id="submit" class="btn btn-success btn-block">Create Account</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
 <script type="text/javascript" src="assets/js/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
