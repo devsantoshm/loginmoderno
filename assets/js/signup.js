922a31d45136cb31502cbf38599a645db94d7d14
SantosHM <dosantoshm@gmail.com>
2019-02-17 11:58:01 -0500

    validar campo name con jquery final

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -3,17 +3,29 @@ $(document).ready(function(){
 	var email    = "";
 	var password = "";
 	var confirm  = "";
+	var name_reg = /^[a-z ]+$/i
 
 	$("#name").focusout(function(){
 		
-		var store = $("#name").val();
+		var store = $.trim($("#name").val());
 		
 		if(store.length == ""){
 			$(".name-error").html("Name is required!");
 			$("#name").addClass("border-red");
+			$("#name").removeClass("border-green");
+			name = ""
 
-		} else {
-			alert(store);
+		} else if(name_reg.test(store)){
+			$(".name-error").html("");
+			$("#name").addClass("border-green");
+			$("#name").removeClass("border-red");
+			name = store
+
+		} else{
+			$(".name-error").html("Integer is not allowd!");
+			$("#name").addClass("border-red");
+			$("#name").removeClass("border-green");
+			name = ""
 		}	
 	})
 })
