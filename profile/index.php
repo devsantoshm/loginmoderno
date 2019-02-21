e035ba3e2ab012f358aed1b678a216cd28b9416f
SantosHM <dosantoshm@gmail.com>
2019-02-21 09:28:12 -0500

    creando el layaout

---
+++ profile/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Profile</title>
+	<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../assets/css/style.css">
+	<link rel="stylesheet" href="../assets/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="../assets/css/profile.css">
+</head>
+<body>
+	<?php include '../parts/nav.php'; ?>
+    <div class="container contents">
+    	<div class="row">
+    		<div class="col-md-3">
+    			<div class="left-area">
+    				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    			</div><!-- left-area -->
+    		</div><!-- col -->
+    		<div class="col-md-9">
+    			<div class="right-area">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia harum qui nulla unde aut dolor velit dolores sequi voluptatibus id, hic, quae quis officiis illo error, reiciendis in? Earum, optio.
+    			</div><!-- right-area -->
+    		</div><!-- col -->
+    	</div><!-- row -->
+    </div><!-- container -->
+
+<script type="text/javascript" src="../assets/js/jquery-3.3.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/profile.js"></script>
+    
+</body>
+</html>
