5e381760f2ae6b06c09d84066649123b91a862f9
SantosHM <dosantoshm@gmail.com>
2019-02-21 10:32:59 -0500

    mostrar menu lateral

--- profile/index.php
+++ profile/index.php
@@ -1,3 +1,5 @@
+<?php include 'functions/func.php'; ?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -14,12 +16,7 @@
     	<div class="row">
     		<div class="col-md-3">
     			<div class="left-area">
-    				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    				<?php links(); ?>
     			</div><!-- left-area -->
     		</div><!-- col -->
     		<div class="col-md-9">
