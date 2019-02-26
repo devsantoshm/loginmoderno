222bd58907e0d22f3f89d72acd61706970d88bc5
SantosHM <dosantoshm@gmail.com>
2019-02-26 13:02:13 -0500

    Add background video

--- index.php
+++ index.php
@@ -16,6 +16,10 @@ if(isset($_SESSION['user_id']))
 	<link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
+
+	<video autoplay muted loop id="myvideo">
+		<source src="assets/img/video.mp4" type="video/mp4">
+	</video>
 	
 	<?php include "parts/nav.php"; ?>
 
@@ -25,104 +29,106 @@ if(isset($_SESSION['user_id']))
         </div>
     <?php endif; ?>
     <?php unset($_SESSION['unutherrized']); ?>
-
-	<div class="container">
-		<div class="row">
-			<div class="col-md-8 content">
-				<?php if(isset($_SESSION['online_user'])): ?>
-					<h3>Thank You <span class="online"><?php echo $_SESSION['online_user'] ?></span> See you next time </h3>
-					<p><i class="fa fa-thumbs-o-up"></i></p>
-				<?php else: ?>
-					<h1>Its always free</h1><hr>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A voluptate dignissimos fuga in dolor quia natus porro blanditiis sint provident dolore libero, deleniti eius voluptatem aperiam vitae quos itaque labore!</p>
-				<?php endif; ?>
-				<?php unset($_SESSION['online_user']); ?>
-			</div>
-			<div class="col-md-4 content">
-				<div class="signup-cover">
-					<div class="card">
-						<div class="card-header">
-							<!-- <div class="row">
-								<div class="col-md-9">
-									<h3 class="form-heading">Signup</h3>
-									<p>Account creating is 100% free so please create the account</p>
-								</div>
-								<div class="col-md-3 text-right">
-									<i class="fa fa-pencil-square-o fa-3x"></i>
-								</div>
-							</div> -->
-						</div>
-						<div class="card-body">
-							<form id="signup_submit">
-								<div class="form-group show-progress">
-									
-								</div>
-								<div class="form-group">
-									<input type="text" name="name" id="name" class="form-control" placeholder="name">
-									<div class="name-error error"></div>
-								</div>
-								<div class="form-group">
-									<input type="email" name="email" id="email" class="form-control" placeholder="email">
-									<div class="email-error error"></div>
-								</div>
-								<div class="form-group">
-									<input type="password" name="password" id="password" class="form-control" placeholder="password">
-									<div class="password-error error"></div>
-								</div>
-								<div class="form-group">
-									<input type="password" id="confirm" class="form-control" placeholder="Confirm Password">
-									<div class="confirm-error error"></div>
-								</div>
-								<div class="form-group">
-									<button type="button" id="submit" class="btn btn-success btn-block form-btn">Create Account</button>
-								</div>
-								<div class="form-group">
-									<a href="#" id="login">Already have an account?</a>
-								</div>
-							</form>
-						</div>
-						<div class="form-icon">
-							<div class="label-heading"><h5>Create New Account</h5></div>
+	
+	<div class="showcase">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 content">
+					<?php if(isset($_SESSION['online_user'])): ?>
+						<h3>Thank You <span class="online"><?php echo $_SESSION['online_user'] ?></span> See you next time </h3>
+						<p><i class="fa fa-thumbs-o-up"></i></p>
+					<?php else: ?>
+						<h1>Its always free</h1><hr>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A voluptate dignissimos fuga in dolor quia natus porro blanditiis sint provident dolore libero, deleniti eius voluptatem aperiam vitae quos itaque labore!</p>
+					<?php endif; ?>
+					<?php unset($_SESSION['online_user']); ?>
+				</div>
+				<div class="col-md-4 content">
+					<div class="signup-cover">
+						<div class="card">
+							<div class="card-header">
+								<!-- <div class="row">
+									<div class="col-md-9">
+										<h3 class="form-heading">Signup</h3>
+										<p>Account creating is 100% free so please create the account</p>
+									</div>
+									<div class="col-md-3 text-right">
+										<i class="fa fa-pencil-square-o fa-3x"></i>
+									</div>
+								</div> -->
+							</div>
+							<div class="card-body">
+								<form id="signup_submit">
+									<div class="form-group show-progress">
+										
+									</div>
+									<div class="form-group">
+										<input type="text" name="name" id="name" class="form-control" placeholder="name">
+										<div class="name-error error"></div>
+									</div>
+									<div class="form-group">
+										<input type="email" name="email" id="email" class="form-control" placeholder="email">
+										<div class="email-error error"></div>
+									</div>
+									<div class="form-group">
+										<input type="password" name="password" id="password" class="form-control" placeholder="password">
+										<div class="password-error error"></div>
+									</div>
+									<div class="form-group">
+										<input type="password" id="confirm" class="form-control" placeholder="Confirm Password">
+										<div class="confirm-error error"></div>
+									</div>
+									<div class="form-group">
+										<button type="button" id="submit" class="btn btn-success btn-block form-btn">Create Account</button>
+									</div>
+									<div class="form-group">
+										<a href="#" id="login">Already have an account?</a>
+									</div>
+								</form>
+							</div>
+							<div class="form-icon">
+								<div class="label-heading"><h5>Create New Account</h5></div>
+							</div>
 						</div>
 					</div>
-				</div>
-				
-				<div class="login-cover">
-					<div class="card">
-						<div class="card-header">
-							<!-- <div class="row">
-								<div class="col-md-9">
-									<h3 class="form-heading">Login</h3>
-									<p>Enter Email and password</p>
-								</div>
-								<div class="col-md-3 text-right">
-									<i class="fa fa-lock fa-3x"></i>
-								</div>
-							</div> -->
-						</div>
-						<div class="card-body">
-							<form id="login-submit-form">
-								<div class="form-group">
-									<div class="login-error error"></div>
-								</div>
-								<div class="form-group">
-									<input type="email" name="login_email" id="login-email" class="form-control" placeholder="email">
-									<div class="login-email-error error"></div>
-								</div>
-								<div class="form-group">
-									<input type="password" name="login_password" id="login-password" class="form-control" placeholder="password">
-									<div class="login-password-error error"></div>
-								</div>
-								<div class="form-group">
-									<button type="button" id="login-submit" class="btn btn-success btn-block form-btn">Login</button>
-								</div>
-								<div class="form-group">
-									<a href="#" id="signup">Create New Account?</a>
-								</div>
-							</form>
-						</div>
-						<div class="form-icon">
-							<div class="label-heading"><h5>User Login</h5></div>
+					
+					<div class="login-cover">
+						<div class="card">
+							<div class="card-header">
+								<!-- <div class="row">
+									<div class="col-md-9">
+										<h3 class="form-heading">Login</h3>
+										<p>Enter Email and password</p>
+									</div>
+									<div class="col-md-3 text-right">
+										<i class="fa fa-lock fa-3x"></i>
+									</div>
+								</div> -->
+							</div>
+							<div class="card-body">
+								<form id="login-submit-form">
+									<div class="form-group">
+										<div class="login-error error"></div>
+									</div>
+									<div class="form-group">
+										<input type="email" name="login_email" id="login-email" class="form-control" placeholder="email">
+										<div class="login-email-error error"></div>
+									</div>
+									<div class="form-group">
+										<input type="password" name="login_password" id="login-password" class="form-control" placeholder="password">
+										<div class="login-password-error error"></div>
+									</div>
+									<div class="form-group">
+										<button type="button" id="login-submit" class="btn btn-success btn-block form-btn">Login</button>
+									</div>
+									<div class="form-group">
+										<a href="#" id="signup">Create New Account?</a>
+									</div>
+								</form>
+							</div>
+							<div class="form-icon">
+								<div class="label-heading"><h5>User Login</h5></div>
+							</div>
 						</div>
 					</div>
 				</div>
